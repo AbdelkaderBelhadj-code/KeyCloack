7866dfaccf40658779c08c8e4c89fe308f54a25b
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-10-18 10:43:08 +0100

    initial commit !

---
+++ src/main/java/com/example/keycloacktest/KeyCloackTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloackTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyCloackTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keycloacktest/KeyCloackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloacktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCloackTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
